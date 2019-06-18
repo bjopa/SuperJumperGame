8830fdf96405d1933ef900d1518e78636355c232
bjopa <bjopa78@gmail.com>
2019-06-18 18:04:17 +0200

    better wall collision control

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,14 @@ public class Main {
 
         //initial status bar print
         String statusBar = "SUPER JUMPER  - STATUS: ALIVE";
-        for (int i = 0; i<statusBar.length(); i++) {
-            terminal.setCursorPosition(i+5,1);
+        for (int i = 0; i < statusBar.length(); i++) {
+            terminal.setCursorPosition(i + 5, 1);
             terminal.putCharacter(statusBar.charAt(i));
         }
         terminal.flush();
 
         //Set up Player 1
-        Player p1 = new Player((terminal.getTerminalSize().getColumns() / 2) - 1, terminal.getTerminalSize().getRows()-2);
+        Player p1 = new Player((terminal.getTerminalSize().getColumns() / 2) - 1, terminal.getTerminalSize().getRows() - 2);
         int oldX = p1.getxPos(), oldY = p1.getyPos();
         int jumpHeight = 7;
         int direction = 1;
@@ -41,24 +41,24 @@ public class Main {
 //        }
 
         int[][] playGround = new int[80][24];
-        for (int col=0; col<playGround.length ; col++) {
-            for (int row=0 ; row<playGround[col].length; row++) {
-                if (col==0 || col == playGround.length-1) playGround[col][row]=1;
+        for (int col = 0; col < playGround.length; col++) {
+            for (int row = 0; row < playGround[col].length; row++) {
+                if (col == 0 || col == playGround.length - 1) playGround[col][row] = 1;
                 else {
-                    if (row==0 || row==playGround[col].length-1) playGround[col][row] = 1;
-                    else if (col>40 && row == 19) playGround[col][row] = 1;
-                    else if (col>36 && col <40 && row == 15) playGround[col][row]=1;
+                    if (row == 0 || row == playGround[col].length - 1) playGround[col][row] = 1;
+                    else if (col > 40 && row == 19) playGround[col][row] = 1;
+                    else if (col > 36 && col < 40 && row == 15) playGround[col][row] = 1;
                     else if (col == 34 && row > 12 && row < 17) playGround[col][row] = 1;
-                    else if (col<35 && row == 17) playGround[col][row] = 1;
-                    else playGround[col][row]=0;
+                    else if (col < 35 && row == 17) playGround[col][row] = 1;
+                    else playGround[col][row] = 0;
                 }
             }
         }
 
-        for (int i = 0 ; i<playGround.length ; i++) {
-            for (int j = 0 ; j < playGround[i].length  ;j++) {
-                if (playGround[i][j]==1) {
-                    terminal.setCursorPosition(i,j);
+        for (int i = 0; i < playGround.length; i++) {
+            for (int j = 0; j < playGround[i].length; j++) {
+                if (playGround[i][j] == 1) {
+                    terminal.setCursorPosition(i, j);
                     terminal.putCharacter('\u2588');
                 }
             }
@@ -112,27 +112,27 @@ public class Main {
                 }
                 case ArrowRight: {
                     direction = 1;
-                    if (p1.getxPos()+direction==terminal.getTerminalSize().getColumns()-1) {
+                    if (p1.getxPos() + direction == terminal.getTerminalSize().getColumns() - 1) {
                         System.out.println("hit wall");
                         break;
                     }
                     p1.setxPos(p1.getxPos() + direction);
                     move(p1, oldX, oldY, terminal/*, obs1*/);
-                    if (playGround[p1.getxPos()][p1.getyPos()+1]==0 && p1.getyPos()!=terminal.getTerminalSize().getRows()-2) {
-                        fall(p1,oldX, oldY,playGround,direction,terminal);
+                    if (playGround[p1.getxPos()][p1.getyPos() + 1] == 0 && p1.getyPos() != terminal.getTerminalSize().getRows() - 2) {
+                        fall(p1, oldX, oldY, playGround, direction, terminal);
                     }
                     break;
                 }
                 case ArrowLeft: {
                     direction = -1;
-                    if (p1.getxPos()+direction==0) {
+                    if (p1.getxPos() + direction == 0) {
                         System.out.println("hit wall");
                         break;
                     }
                     p1.setxPos(p1.getxPos() + direction);
                     move(p1, oldX, oldY, terminal/*, obs1*/);
-                    if (playGround[p1.getxPos()][p1.getyPos()+1]==0 && p1.getyPos()!=terminal.getTerminalSize().getRows()-2) {
-                        fall(p1,oldX, oldY,playGround,direction,terminal);
+                    if (playGround[p1.getxPos()][p1.getyPos() + 1] == 0 && p1.getyPos() != terminal.getTerminalSize().getRows() - 2) {
+                        fall(p1, oldX, oldY, playGround, direction, terminal);
                     }
                     break;
                 }
@@ -147,7 +147,7 @@ public class Main {
             oldX = p.getxPos();
             oldY = p.getyPos();
 
-            if (playGround[p.getxPos()][p.getyPos()-1]==1) {
+            if (playGround[p.getxPos()][p.getyPos() - 1] == 1) {
                 System.out.println("hit ceiling");
                 break;
             }
@@ -156,7 +156,7 @@ public class Main {
 
             //check hit wall on jump
             if (i % 3 == 0)
-                if (p.getxPos()+direction!=0 && p.getxPos()+direction!=terminal.getTerminalSize().getColumns()-1)
+                if (playGround[p.getxPos() + direction][p.getyPos()] == 0)
                     p.setxPos(p.getxPos() + direction);
 
             terminal.setBackgroundColor(TextColor.ANSI.DEFAULT);
@@ -166,15 +166,15 @@ public class Main {
             terminal.setCursorPosition(p.getxPos(), p.getyPos());
             terminal.putCharacter('O');
             terminal.flush();
-            Thread.sleep(12+i*2);
+            Thread.sleep(12 + i * 2);
         }
 
-        int currentHeight = (terminal.getTerminalSize().getRows()-p.getyPos());
+        int currentHeight = (terminal.getTerminalSize().getRows() - p.getyPos());
         for (int i = currentHeight; i > 0; i--) {
             oldX = p.getxPos();
             oldY = p.getyPos();
 
-            if (playGround[p.getxPos()][p.getyPos()+1]==1) {
+            if (playGround[p.getxPos()][p.getyPos() + 1] == 1) {
                 System.out.println("hit floor");
                 break;
             }
@@ -182,7 +182,7 @@ public class Main {
             p.setyPos(p.getyPos() + 1);
             //check hit wall on jump
             if (i % 3 == 0)
-                if (p.getxPos()+direction!=0 && p.getxPos()+direction!=terminal.getTerminalSize().getColumns()-1)
+                if (playGround[p.getxPos() + direction][p.getyPos()] == 0)
                     p.setxPos(p.getxPos() + direction);
 
             terminal.setBackgroundColor(TextColor.ANSI.DEFAULT);
@@ -192,7 +192,7 @@ public class Main {
             terminal.setCursorPosition(p.getxPos(), p.getyPos());
             terminal.putCharacter('O');
             terminal.flush();
-            Thread.sleep(12+(int)(i/4));
+            Thread.sleep(12 + (int) (i / 4));
         }
     }
 
@@ -206,14 +206,14 @@ public class Main {
         terminal.flush();
     }
 
-    public static void fall(Player p, int oldX, int oldY, int[][]playGround, int direction, Terminal terminal) throws Exception {
-        int currentHeight = (terminal.getTerminalSize().getRows()-p.getyPos());
+    public static void fall(Player p, int oldX, int oldY, int[][] playGround, int direction, Terminal terminal) throws Exception {
+        int currentHeight = (terminal.getTerminalSize().getRows() - p.getyPos());
 
         for (int i = currentHeight; i > 0; i--) {
             oldX = p.getxPos();
             oldY = p.getyPos();
 
-            if (playGround[p.getxPos()][p.getyPos()+1]==1) {
+            if (playGround[p.getxPos()][p.getyPos() + 1] == 1) {
                 System.out.println("hit floor");
                 break;
             }
@@ -228,16 +228,16 @@ public class Main {
             terminal.setCursorPosition(p.getxPos(), p.getyPos());
             terminal.putCharacter('O');
             terminal.flush();
-            Thread.sleep(12+(int)(i/4));
+            Thread.sleep(12 + (int) (i / 4));
         }
     }
 
     public static void collisionChecker(Player p1, Obstacle obs1, Terminal terminal) throws Exception {
-        if (obs1.getxPos()==p1.getxPos() && obs1.getyPos()==p1.getyPos()) {
+        if (obs1.getxPos() == p1.getxPos() && obs1.getyPos() == p1.getyPos()) {
             terminal.setBackgroundColor(TextColor.ANSI.DEFAULT);
             String deathMess = "-DEAD-";
-            for (int i = 0; i<deathMess.length(); i++) {
-                terminal.setCursorPosition(i+29,1);
+            for (int i = 0; i < deathMess.length(); i++) {
+                terminal.setCursorPosition(i + 29, 1);
                 terminal.putCharacter(deathMess.charAt(i));
             }
             terminal.flush();
