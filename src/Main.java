284581b5a25ef01ca19d369678a3db991ce357f5
bjopa <bjopa78@gmail.com>
2019-06-19 17:59:26 +0200

    Added more elements (lava, spikes)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import org.w3c.dom.Text;
+
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,16 +16,9 @@ public class Main {
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false); //size 80x24
 
-        //initial status bar print
-        String statusBar = "SUPER JUMPER  - STATUS: ALIVE";
-        for (int i = 0; i < statusBar.length(); i++) {
-            terminal.setCursorPosition(i + 5, 1);
-            terminal.putCharacter(statusBar.charAt(i));
-        }
-        terminal.flush();
 
         //Set up Player 1
-        Player p1 = new Player((terminal.getTerminalSize().getColumns() / 2) - 1, terminal.getTerminalSize().getRows() - 2);
+        Player p1 = new Player((terminal.getTerminalSize().getColumns() / 2) - 1, terminal.getTerminalSize().getRows() - 2, 1);
         int oldX = p1.getxPos(), oldY = p1.getyPos();
         int jumpHeight = 7;
         int direction = 1;
@@ -30,29 +26,14 @@ public class Main {
 //        //Set up Enemy
 //        Enemy enemy1 = new Enemy(terminal.getTerminalSize().getColumns()-2, 22, 1);
 
-        //Set up initial level
+        //Set up levels, allocate initial lvl
         Level level1 = new Level("Level1.txt");
         int[][] playGround = level1.getLevelDesign();
 
-        //Draw level
-        for (int i = 0; i < playGround.length; i++) {
-            for (int j = 0; j < playGround[i].length; j++) {
-                if (playGround[i][j] == 1) {
-                    terminal.setCursorPosition(i, j);
-                    terminal.putCharacter('\u2588');
-                }
-            }
-        }
-        terminal.flush();
-
-        terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
-        terminal.setBackgroundColor(TextColor.ANSI.GREEN);
-        terminal.setCursorPosition(p1.getxPos(), p1.getyPos());
-        terminal.putCharacter('O');
+        //draw enemy
 //        terminal.setBackgroundColor(TextColor.ANSI.RED);
 //        terminal.setCursorPosition(enemy1.getxPos(),enemy1.getyPos());
 //        terminal.putCharacter('O');
-        terminal.flush();
 
         //Set up KeyStroke variables
         KeyStroke keyStroke;
@@ -61,6 +42,58 @@ public class Main {
 
         //running it...
         while (continueReadingInput) {
+
+            if (p1.getNewGame() == 1) {
+                //STARTUP things
+
+                //Draw level
+                for (int i = 0; i < playGround.length; i++) {
+                    for (int j = 0; j < playGround[i].length; j++) {
+
+                        char blockType;
+                        switch (playGround[i][j]) {
+                            case 1:
+                                terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+                                terminal.setBackgroundColor(TextColor.ANSI.BLACK);
+                                blockType = '\u2588';
+                                break;
+                            case 2:
+                                terminal.setForegroundColor(TextColor.ANSI.YELLOW);
+                                terminal.setBackgroundColor(TextColor.ANSI.RED);
+                                blockType = '\u25B2';
+                                break;
+                            case 3:
+                                terminal.setForegroundColor((TextColor.ANSI.WHITE));
+                                terminal.setBackgroundColor(TextColor.ANSI.BLACK);
+                                blockType = '\u25B2';
+                                break;
+                            default:
+                                continue;
+                        }
+                        terminal.setCursorPosition(i, j);
+                        terminal.putCharacter(blockType);
+                    }
+                }
+
+                //initial status bar print
+                terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+                terminal.setBackgroundColor(TextColor.ANSI.BLUE);
+                String statusBar = "SUPER JUMPER  - STATUS: ALIVE";
+                for (int i = 0; i < statusBar.length(); i++) {
+                    terminal.setCursorPosition(i + 5, 0);
+                    terminal.putCharacter(statusBar.charAt(i));
+                }
+                terminal.flush();
+
+
+                terminal.setForegroundColor(TextColor.ANSI.DEFAULT);
+                terminal.setBackgroundColor(TextColor.ANSI.GREEN);
+                terminal.setCursorPosition(p1.getxPos(), p1.getyPos());
+                terminal.putCharacter('O');
+                terminal.flush();
+                p1.setNewGame(0);
+            }
+
             do {
                 Thread.sleep(5);
                 keyStroke = terminal.pollInput();
@@ -92,7 +125,7 @@ public class Main {
                 }
                 case ArrowRight: {
                     direction = 1;
-                    if (playGround[p1.getxPos()+direction][p1.getyPos()]==1) {
+                    if (playGround[p1.getxPos() + direction][p1.getyPos()] == 1) {
                         break;
                     }
                     p1.setxPos(p1.getxPos() + direction);
@@ -104,7 +137,7 @@ public class Main {
                 }
                 case ArrowLeft: {
                     direction = -1;
-                    if (playGround[p1.getxPos()+direction][p1.getyPos()]==1) {
+                    if (playGround[p1.getxPos() + direction][p1.getyPos()] == 1) {
                         break;
                     }
                     p1.setxPos(p1.getxPos() + direction);
@@ -125,7 +158,8 @@ public class Main {
             oldX = p.getxPos();
             oldY = p.getyPos();
 
-            if (playGround[p.getxPos()][p.getyPos() - 1] == 1) {
+            //check hit ceiling on jump
+            if (playGround[p.getxPos()][p.getyPos() - 1] != 0) {
                 break;
             }
 
@@ -151,7 +185,34 @@ public class Main {
             oldX = p.getxPos();
             oldY = p.getyPos();
 
+            //check hit floor or lava on falling from jump
             if (playGround[p.getxPos()][p.getyPos() + 1] == 1) {
+                break;
+            } else if (playGround[p.getxPos()][p.getyPos() + 1] >= 1) {
+                terminal.setBackgroundColor(TextColor.ANSI.DEFAULT);
+                terminal.setCursorPosition(oldX, oldY);
+                terminal.putCharacter(' ');
+                terminal.setBackgroundColor(TextColor.ANSI.GREEN);
+                terminal.setCursorPosition(p.getxPos(), p.getyPos());
+                terminal.putCharacter('O');
+                terminal.flush();
+                switch (playGround[p.getxPos()][p.getyPos() + 1]) {
+                    case 2:
+                        p.setyPos(p.getyPos() + 1);
+                        terminal.setBackgroundColor(TextColor.ANSI.DEFAULT);
+                        terminal.setCursorPosition(oldX, oldY);
+                        terminal.putCharacter(' ');
+                        terminal.setBackgroundColor(TextColor.ANSI.GREEN);
+                        terminal.setCursorPosition(p.getxPos(), p.getyPos());
+                        terminal.putCharacter('O');
+                        terminal.flush();
+                        endgame("LAVA", p, terminal);
+                        p.setNewGame(1);
+                        break;
+                    case 3:
+                        endgame("SPIKES", p, terminal);
+                        break;
+                }
                 break;
             }
 
@@ -216,6 +277,26 @@ public class Main {
                 terminal.putCharacter(deathMess.charAt(i));
             }
             terminal.flush();
+        }
+    }
+
+    public static void endgame(String reason, Player p, Terminal terminal) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You died from " + reason);
+        System.out.println("Would you like to play again (y/n)?: ");
+        String choice = sc.nextLine();
+        switch (choice) {
+            case "y":
+            case "Y":
+                System.out.println("Restarting...");
+                //Player p1 = new Player((terminal.getTerminalSize().getColumns() / 2) - 1, terminal.getTerminalSize().getRows() - 2);
+                p.setxPos((terminal.getTerminalSize().getColumns() / 2) - 1);
+                p.setyPos((terminal.getTerminalSize().getRows()) - 2);
+                int oldX = p.getxPos(), oldY = p.getyPos();
+                break;
+            default:
+                break;//terminal.close()
+
         }
     }
 
--- src/Player.java
+++ src/Player.java
@@ -2,10 +2,13 @@ public class Player {
 
     private int xPos;
     private int yPos;
+    private int newGame;
 
-    public Player(int xPos, int yPos) {
+    public Player(int xPos, int yPos, int newGame) {
         this.xPos = xPos;
-        this.yPos = yPos; }
+        this.yPos = yPos;
+        this.newGame = newGame;
+    }
 
     public int getxPos() {
         return xPos;
@@ -23,4 +26,11 @@ public class Player {
         this.yPos = yPos;
     }
 
+    public int getNewGame() {
+        return newGame;
+    }
+
+    public void setNewGame(int newGame) {
+        this.newGame = newGame;
+    }
 }
