cc073b891b5f3db1bcf24ba1e6340af303dcf5ef
bjopa <bjopa78@gmail.com>
2019-06-19 10:03:43 +0200

    fixed reading to intArray

--- src/Level.java
+++ src/Level.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Level {
 
-    int[][] levelDesign = new int[80][24];
-    String levelString;
+    private int[][] levelDesign = new int[24][80];
+    private String[][] levelAsString = new String[24][80];
+    private String levelString;
 
-    public Level(String filename) throws Exception{
+    Level(String filename) throws Exception{
         this.levelString=readLevelFile(filename);
         stringToArray(this.levelString);
     }
 
-    public String readLevelFile(String filename) throws Exception{
+    private String readLevelFile(String filename) throws Exception{
         BufferedReader reader = Files.newBufferedReader(Paths.get(filename));
         return reader.lines().collect(Collectors.joining(System.lineSeparator()));
     }
@@ -23,10 +23,17 @@ public class Level {
         return levelString;
     }
 
-    public void stringToArray(String levelString) {
-        String[] tempString = levelString.split("\n",0);
-        System.out.println(tempString[16]);
-        //TODO Add string splitter on &
+    private void stringToArray(String levelString) {
+
+        String[] splitString = levelString.split("\r\n",0);
+
+        for (int i = 0 ; i < splitString.length; i++)
+            levelAsString[i] = splitString[i].split(";", 0);
+
+        for (int i = 0; i < levelAsString.length ; i++)
+            for (int j = 0; j < levelAsString[i].length; j++)
+                levelDesign[i][j] = Integer.parseInt(levelAsString[i][j]);
+
     }
 
 }
